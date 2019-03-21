c03fa10c407a899b6832b3f8ee49c40590f17436
Nicolai Stølen <35232838+nicolst@users.noreply.github.com>
2019-03-21 21:13:42 +0100

    Initial commit

---
+++ main.ino
@@ -0,0 +1,79 @@
+#include <math.h>
+
+const int stepping[8][4] = {{0, 1, 1, 1}, {0, 0, 1, 1}, 
+                            {1, 0, 1, 1}, {1, 0, 0, 1}, 
+                            {1, 1, 0, 1}, {1, 1, 0, 0}, 
+                            {1, 1, 1, 0}, {0, 1, 1, 0}};
+
+class Motor {
+  int a, b, c, d;
+  int stepIndex = 0;
+
+public:
+  Motor(int a, int b, int c, int d) : a(a), b(b), c(c), d(d) {
+    pinMode(a, OUTPUT);
+    pinMode(b, OUTPUT);
+    pinMode(c, OUTPUT);
+    pinMode(d, OUTPUT);  
+  }
+  
+  void eightStep() {
+    digitalWrite(a, stepping[stepIndex][0]);      
+    digitalWrite(b, stepping[stepIndex][1]);
+    digitalWrite(c, stepping[stepIndex][2]);
+    digitalWrite(d, stepping[stepIndex][3]);
+    stepIndex = (stepIndex + 1) % 8;
+  }
+
+  void fullStep(int d) {
+    for (int i = 0; i < 8; i++) {
+      eightStep();
+      delay(d);
+    }
+  }
+
+  void rotate(int deg) {
+    int steps = int(deg * 512.0 / 360.0);
+    for (int i = 0; i < steps; i++) fullStep(1);
+  }
+  
+};
+
+
+//int a = 9, b = 8, c = 11, d = 10;
+//int a = 5, b= 4, c = 7, d = 6;
+
+Motor mot1(9, 8, 11, 10);
+Motor mot2(5, 4, 7, 6);
+ 
+void setup() {
+  // initialize the serial port:
+  Serial.begin(9600);
+  //mot1 = Motor::Motor(9, 8, 11, 10);
+  //mot2 = Motor::Motor(5, 4, 7, 6);
+  delay(1);
+}
+ 
+//void rotate(float degress){
+// 
+//}
+ 
+void loop() {
+  if (Serial.available() > 0) {
+    int command = Serial.parseInt();
+    if (command == 0) return;
+    
+    Motor* m = command > 0 ? &mot1 : &mot2;
+    command = abs(command);
+    bool degrees = !((command & 0x8000) >> 15);
+
+    int value = command & 0x0FFF;
+    
+    if (degrees) {
+      m->rotate(value);
+    } else {
+      for (int i = 0; i < value; i++) m->fullStep(1);
+    }
+    
+  }  
+}
